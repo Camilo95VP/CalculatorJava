4c2aeafb27192876ac8b852871a749502c866358
JuanC <camiloflstudio@gmail.com>
2022-07-05 21:35:01 -0500

    Validaciones

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
         boolean estado;
         double n1=0;
         double n2=0;
+
         do {
             System.out.println("OPERACIONES :\n 1.Sumar\n 2.Restar\n 3.Multiplicar\n 4.Dividir\n 5.Modulo\n 6.Salir");
             Scanner leerOpcion = new Scanner(System.in);
@@ -17,37 +18,37 @@ public class Calculator {
             switch (opcion) {
 
                 case 1:
-
+                    System.out.println("SUMAR");
                     System.out.println("Digite el primer numero: ");
                     n1 = leerNumeros.nextDouble();
                     System.out.println("Digite el segundo numero: ");
                     n2 = leerNumeros.nextDouble();
                     double resultadoSuma = n1 + n2;
-                    System.out.println(n1 + " + " + n2 + " = " + resultadoSuma);
+                    System.out.println(n1 + " + " + n2 + " = " + resultadoSuma + "\n");
                     break;
 
                 case 2:
-
+                    System.out.println("RESTAR");
                     System.out.println("Digite el primer numero: ");
                     n1= leerNumeros.nextDouble();
                     System.out.println("Digite el segundo numero: ");
                     n2 = leerNumeros.nextDouble();
                     double resultadoResta = n1 - n2;
-                    System.out.println(n1 + " - " + n2 + " = " + resultadoResta);
+                    System.out.println(n1 + " - " + n2 + " = " + resultadoResta+"\n");
                     break;
 
                 case 3:
-
+                    System.out.println("MULTIPLICAR");
                     System.out.println("Digite el primer numero: ");
                     n1 = leerNumeros.nextDouble();
                     System.out.println("Digite el segundo numero: ");
                     n2 = leerNumeros.nextDouble();
                     double resultadoMulti = n1 * n2;
-                    System.out.println(n1 + " * " + n2 + " = " + resultadoMulti);
+                    System.out.println(n1 + " * " + n2 + " = " + resultadoMulti+"\n");
                     break;
 
                 case 4:
-
+                    System.out.println("DIVIDIR");
                     System.out.println("Digite el numerador: ");
 
                     n1 = leerNumeros.nextDouble();
@@ -55,29 +56,31 @@ public class Calculator {
                     n2= leerNumeros.nextDouble();
 
                     if (n2 == 0) {
-                        System.out.println("No se puede dividir entre 0");
+                        System.out.println("El divisor no debe ser 0\n");
                         break;
                     }
 
                     double resultadoDiv = n1 / n2;
-                    System.out.println(n1 + " / " + n2 + " = " + resultadoDiv);
+                    System.out.println(n1 + " / " + n2 + " = " + resultadoDiv+"\n");
                     break;
 
                 case 5:
+                    System.out.println("MODULO");
                     System.out.println("Digite el primer numero: ");
                     n1 = leerNumeros.nextDouble();
                     System.out.println("Digite el segundo numero: ");
                     n2 = leerNumeros.nextDouble();
                     if (n2 == 0) {
-                        System.out.println("El divisor no debe ser 0");
+                        System.out.println("El divisor no debe ser 0\n");
                         break;
                     }
 
                     double resultadoModulo=n1%n2;
-                    System.out.println(n1 + " % " + n2 + " = " + resultadoModulo);
+                    System.out.println(n1 + " % " + n2 + " = " + resultadoModulo+"\n");
                     break;
 
                 case 6:
+                    System.out.println("HASTA PRONTO!");
                     estado = false;
             }
 
